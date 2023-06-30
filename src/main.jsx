a344fcc6432d633f9009f601a6c7330d646d753e
luthfiahmdf <luthfiahmdf@gmail.com>
2023-06-30 22:46:01 +0700

    deleting

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "animate.css/animate.min.css";
+
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./index.css";
 
